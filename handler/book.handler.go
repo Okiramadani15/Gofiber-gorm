106d3dce7206c54a18b60c0abef91607c8469a67
Oki Ramadani <oki.ramadani@delameta.com>
2024-08-07 11:50:53 +0700

    [FII]

--- handler/book.handler.go
+++ handler/book.handler.go
@@ -1,9 +1,11 @@
 package handler
 
 import (
+	"fmt"
 	"go_fiber_gorm/database"
 	"go_fiber_gorm/model/entity"
 	"go_fiber_gorm/model/request"
+	"log"
 
 	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
@@ -26,10 +28,28 @@ func BookHandlerCreate(ctx *fiber.Ctx) error {
 		})
 	}
 
+	// HANDLE FILE
+	file, errFile := ctx.FormFile("cover")
+	if errFile != nil {
+		log.Println("Error File =", errFile)
+	}
+
+	var filename string
+	if file != nil {
+		filename = file.Filename
+
+		errSaveFile := ctx.SaveFile(file, fmt.Sprintf("./public/covers/%s", filename))
+		if errSaveFile != nil {
+			log.Println("Fail to store file into public/covers directory.")
+		}
+	} else {
+		log.Println("nothing file to be uploading")
+	}
+
 	newBook := entity.Book{
 		Title:  book.Title,
 		Author: book.Author,
-		Cover:  book.Cover,
+		Cover:  filename,
 	}
 
 	errCreateBook := database.DB.Create(&newBook).Error
--- model/entity/book.entity.go
+++ model/entity/book.entity.go
@@ -9,7 +9,7 @@ import (
 type Book struct {
 	ID        uint           `json:"id" gorm:"primaryKey"`
 	Title     string         `json:"title"`
-	Author    string         `json:"author "`
+	Author    string         `json:"author"`
 	Cover     string         `json:"cover"`
 	CreatedAt time.Time      `json:"created_at"`
 	UpdatedAt time.Time      `json:"updated_at"`
