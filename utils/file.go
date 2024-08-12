dbb107644c9e08192a1c6305bfa276624745f989
Oki Ramadani <oki.ramadani@delameta.com>
2024-08-12 17:19:06 +0700

    [fix] Validasi tipe data

--- utils/file.go
+++ utils/file.go
@@ -1,8 +1,10 @@
 package utils
 
 import (
+	"errors"
 	"fmt"
 	"log"
+	"mime/multipart"
 	"os"
 	"path/filepath"
 
@@ -20,26 +22,38 @@ const DefaultpathAssetImage = "./public/covers"
 // 		})
 // 	}
 
-// 	var filename *string
+// 	var filename string
 // 	if file != nil {
-// 		filename = &file.Filename
-// 		extensionFile := filepath.Ext(*filename)
-// 		newFilename := fmt.Sprintf("gambar-satu%s", extensionFile)
+// 		errCheckContentType := CheckContentType(file, "img/jpg", "img/png", "img/gif")
+// 		if errCheckContentType != nil {
+// 			return ctx.Status(fiber.StatusUnprocessableEntity).JSON(fiber.Map{
+// 				"message": errCheckContentType.Error(),
+// 			})
+// 		}
 
-// 		errSaveFile := ctx.SaveFile(file, fmt.Sprintf("./public/covers/%s", newFilename))
+// 		extensionFile := filepath.Ext(file.Filename)
+// 		// Buat nama file baru
+// 		filename = fmt.Sprintf("gambar-satu%s", extensionFile)
+
+// 		// Simpan file ke direktori yang diinginkan
+// 		errSaveFile := ctx.SaveFile(file, fmt.Sprintf("./public/covers/%s", filename))
 // 		if errSaveFile != nil {
 // 			log.Println("Failed to store file into public/covers directory:", errSaveFile)
-// 			// return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-// 			// 	"error": "Failed to save the file",
-// 			// })
+// 			return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+// 				"error":   "Failed to save the file",
+// 				"message": "failed",
+// 			})
 // 		}
+// 		// Ambil extension file
 // 	} else {
 // 		log.Println("No file to be uploaded")
-// 		// return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-// 		// 	"error": "No file uploaded",
-// 		// })
+// 		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+// 			"error":   "No file uploaded",
+// 			"message": "failed",
+// 		})
 // 	}
 
+// 	// Simpan nama file ke dalam context locals sebagai string
 // 	ctx.Locals("filename", filename)
 
 // 	return ctx.Next()
@@ -56,7 +70,14 @@ func HandleSingleFile(ctx *fiber.Ctx) error {
 
 	var filename string
 	if file != nil {
-		// Ambil extension file
+		// Periksa apakah Content-Type sesuai dengan jpg, png, atau gif
+		errCheckContentType := CheckContentType(file, "image/jpeg", "image/png", "image/gif")
+		if errCheckContentType != nil {
+			return ctx.Status(fiber.StatusUnprocessableEntity).JSON(fiber.Map{
+				"message": errCheckContentType.Error(),
+			})
+		}
+
 		extensionFile := filepath.Ext(file.Filename)
 		// Buat nama file baru
 		filename = fmt.Sprintf("gambar-satu%s", extensionFile)
@@ -98,10 +119,6 @@ func HandleMultipleFile(ctx *fiber.Ctx) error {
 		if file != nil {
 			extensionFile := filepath.Ext(file.Filename)
 			filename = fmt.Sprintf("%d-%s%s", i, "gambar", extensionFile)
-			// // Ambil extension file
-			// extensionFile := filepath.Ext(file.Filename)
-			// // Buat nama file baru
-			// filename = fmt.Sprintf("gambar-satu%s", extensionFile)
 
 			errSaveFile := ctx.SaveFile(file, fmt.Sprintf("./public/covers/%s", filename))
 			if errSaveFile != nil {
@@ -141,4 +158,19 @@ func HandleRemoveFile(filename string, pathFile ...string) error {
 	}
 
 	return nil
+}
+
+func CheckContentType(file *multipart.FileHeader, allowedContentTypes ...string) error {
+	contentType := file.Header.Get("Content-Type")
+	for _, allowedType := range allowedContentTypes {
+		if contentType == allowedType {
+			return nil // Jika tipe konten sesuai, return nil
+		}
+	}
+
+	return errors.New("not allowed file  type")
+	// } else {
+	// 	return errors.New("Not found content type to be checking")
+	// }
+
 }
